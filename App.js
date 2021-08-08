8bef78a72403689edf4302572aecd9bab251c153
Fotios Tsakiris <footios76@gmail.com>
2021-08-08 18:06:31 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,70 +1,26 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import AddList from './components/AddList';
-import ItemsList from './components/ItemsList';
-import Error from './components/ErrorComponent';
+import React from "react";
 
-export default function App() {
-  const [listInput, setListInput] = useState('');
-  const [list, setList] = useState([]);
-  const [error, setError] = useState(false);
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-  const handleInput = (text) => {
-    setListInput(text);
-  };
+import SignIn from "./src/screens/SignIn";
+import Example from "./src/screens/Example";
 
-  const handleSetList = () => {
-    if (!listInput) {
-      setError(true);
-      return;
-    }
+const Stack = createStackNavigator();
 
-    const newInput = {
-      id: Math.random() * 1000,
-      value: listInput,
-    };
-
-    setList([...list, newInput]);
-    setListInput('');
-    setError(false);
-  };
-
-  const handleDelete = (id) => {
-    setList(list.filter((item) => item.id !== id));
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.inputArea}>
-        <AddList
-          onPressAdd={handleSetList}
-          value={listInput}
-          onChange={handleInput}
-        />
-      </View>
-      <View style={styles.listArea}>
-        <ItemsList data={list} onDelete={handleDelete} />
-        <Error visible={error}>Please insert a valid text</Error>
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  inputArea: {
-    flex: 2,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  listArea: {
-    flex: 8,
-    paddingTop: 20,
-    alignItems: 'center',
-    width: '80%',
-    marginVertical: 15,
-  },
-});
+export default () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ title: "Sign In" }}
+      />
+      <Stack.Screen
+        name="App"
+        component={Example}
+        options={{ title: "Success!" }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
